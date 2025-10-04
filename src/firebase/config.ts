421b7f8ffdaad252bdbe28d5cc77a0c679425751
cap64r1q7wh nikhil <cap64r1q7whnikhil@viva-technology.org>
2025-10-04 15:58:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6856413441-9ef50",
+  "appId": "1:556048751811:web:bd44e3d7a3e0cbae5d1676",
+  "apiKey": "AIzaSyA_T-7NA1KSfloxqwRPSlANWAEB-cOpQuw",
+  "authDomain": "studio-6856413441-9ef50.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "556048751811"
+};
